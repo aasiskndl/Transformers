7271c53279ecdf3699447ceddc42ebe8d2535d21
aasiskndl <aashishkandel90@gmail.com>
2026-01-21 13:00:57 +0545

    Added projection layer

--- model.py
+++ model.py
@@ -187,3 +187,12 @@ class Decoder(nn.Module):
             x = layer(x, encoder_output, src_mask, tgt_mask)
         return self.norm(x)
 
+class ProjectionLayer(nn.Module):
+
+    def __init__(self, d_model, vocab_size) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x) -> None:
+        # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+        return self.proj(x)
