beef1c698a4f7b56c4d953fc8ab2e10cb5c82683
aasiskndl <aashishkandel90@gmail.com>
2026-01-27 22:08:28 +0545

    Made minor changes in train.py and config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 8,
-        "num_epochs": 20,
+        "num_epochs": 5,
         "lr": 10**-4,
         "seq_len": 350,
-        "d_model": 512,
-        "datasource": 'opus_books',
+        "d_model": 256,
+        "datasource": 'Helsinki-NLP/opus_books',
         "lang_src": "en",
         "lang_tgt": "it",
         "model_folder": "weights",
